5055e20d0858ad9c2ebf4d733cad6e833e768889
ManishBailwal <102870593+ManishBailwal@users.noreply.github.com>
2024-08-04 17:53:05 +0530

    made image remove functionality

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let profilePic = document.getElementById("profile-pic")
 
 let inputFile = document.getElementById("input-file")
 
+let btn = document.getElementById("btn");
+
 inputFile.onchange = function(){
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
+}
+
+btn.onclick = function(){
+
+    profilePic.src = "";
 }
